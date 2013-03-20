bdbbe9086714fea7831192b52cc9ebfa39754be5
will <will.mobile.hd@gmail.com>
2013-03-19 22:01:22 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Om::Application.config.secret_token = 'bbb7fe4aeb6193be173c74e038938c9d4eb7af2748f88f8eae5d8052a095ede2701672ad8c75980dbb188bbb1c29ff61ab05b58c019444c04ee3b70e080cbdd0'
